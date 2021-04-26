908525c51dfbc34f758db371794869041b5db6d6
fbemb <f.bembassat@gmail.com>
2021-04-26 17:13:46 +0200

    Associer le click droit et gauche

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const NUMBER_OF_MINES = 10
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
 // Associe la grille à la classe 'board'
 const boardElement = document.querySelector('.board')
+const minesLeftText = document.querySelector('[data-mine-count]')
 
 console.log(board)
 
@@ -16,15 +17,25 @@ console.log(board)
 board.forEach(row => {
     row.forEach(tile => {
         boardElement.append(tile.element)
+        // Evenement pour le click gauche
+        tile.element.addEventListener('click', () => {
+
+        })
+        // Evenement pour le click droit
+        tile.element.addEventListener('contextmenu', e => {
+            // Empeche d'ouvrir le menu classique du click droit
+            e.preventDefault()
+        })
     })
 })
 
 // Associe la config du css à la grille
 boardElement.style.setProperty('--size', BOARD_SIZE)
+minesLeftText.textContent = NUMBER_OF_MINES
 
 // 1. Remplir le tableau avec des tuiles/mines = V
-// 2. Click gauche sur les tuiles
+// 2. Click gauche sur les tuiles = V
     // a. Dévoiler les tuiles
-// 3. Click droit sur les tuiles
+// 3. Click droit sur les tuiles = V
     // a. Marquer les tuiles
 // 4. Vérifier quand on gagne/perd
