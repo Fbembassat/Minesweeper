7925e35f12a8c0bc6a018b439eafb583aa2809ba
fbemb <f.bembassat@gmail.com>
2021-04-26 17:03:49 +0200

    Placement des mines alétoirement

--- minesweeper.js
+++ minesweeper.js
@@ -10,6 +10,9 @@ const TILE_STATUSES = {
 
 export function createBoard(boardSize, numberOfMines) {
     const board = []
+    const minePositions = getMinePositions(boardSize, numberOfMines)
+    console.log(minePositions)
+
     // parcourir le tableau dans la direction x et y pour créer une grille de jeu démineur
     for (let x = 0; x < boardSize; x++) {
         const row = []
@@ -18,12 +21,19 @@ export function createBoard(boardSize, numberOfMines) {
             const element = document.createElement('div')
             // Met l'élément par défaut en 'caché'
             element.dataset.status = TILE_STATUSES.HIDDEN
-            
-            // crée un objet qui associe chaque tuile à ce qu'on y met
+
+            // crée un objet qui attribue chaque tuile à ce qu'on y met
             const tile = {
                 element,
                 x,
                 y,
+                mine: minePositions.some(positionMatch.bind(null, { x, y })),
+                get status() {
+                    return this.element.dataset.status
+                },
+                set status(value) {
+                    this.element.dataset.status = value
+                }
             }
 
             row.push(tile)
@@ -32,4 +42,31 @@ export function createBoard(boardSize, numberOfMines) {
     }
 
     return board
+}
+
+// Fonction qui crée aléatoirement des mines 
+function getMinePositions(boardSize, numberOfMines) {
+    const positions = []
+
+    while (positions.length < numberOfMines) {
+        const position = {
+            x: randomNumber(boardSize),
+            y: randomNumber(boardSize)
+        }
+
+        if (!positions.some(positionMatch.bind(null, position))) {
+            positions.push(position)
+        }
+    }
+
+    return positions
+}
+
+function positionMatch(a, b) {
+    return a.x === b.x && a.y === b.y
+}
+
+// Fonction qui crée un nombre aléatoire et en fait un int
+function randomNumber(size) {
+    return Math.floor(Math.random() * size)
 }
--- script.js
+++ script.js
@@ -3,13 +3,14 @@
 import { createBoard } from './minesweeper.js'
 
 const BOARD_SIZE = 10
-const NUMBER_OF_MINES = 2
+const NUMBER_OF_MINES = 10
 
 const board = createBoard(BOARD_SIZE, NUMBER_OF_MINES)
 // Associe la grille à la classe 'board'
 const boardElement = document.querySelector('.board')
-// Associe la config du css à la grille
-boardElement.style.setProperty('--size', BOARD_SIZE)
+
+console.log(board)
+
 
 // Crée une boucle qui crée chaque tuile, les 2 foreach car on a un tableau a 2 dimensions
 board.forEach(row => {
@@ -18,7 +19,10 @@ board.forEach(row => {
     })
 })
 
-// 1. Remplir le tableau avec des tuiles/mines
+// Associe la config du css à la grille
+boardElement.style.setProperty('--size', BOARD_SIZE)
+
+// 1. Remplir le tableau avec des tuiles/mines = V
 // 2. Click gauche sur les tuiles
     // a. Dévoiler les tuiles
 // 3. Click droit sur les tuiles
